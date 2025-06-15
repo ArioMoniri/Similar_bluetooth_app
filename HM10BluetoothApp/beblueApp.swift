25e4fe4e83965d3ffcb65cbd59f386af9522c422
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2025-06-15 19:57:08 +0300

    Update and rename HM10BluetoothAppApp.swift to beblueApp.swift

---
+++ HM10BluetoothApp/beblueApp.swift
@@ -0,0 +1,17 @@
+//
+//  beblueApp.swift
+//  beblue
+//
+//  Created by Ariorad Moniri on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct beblueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DeviceListView()
+        }
+    }
+}
